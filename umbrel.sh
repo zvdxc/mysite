ffbff91414b3413cba11fb79c9a38caccd20a6b7
zvdxc <88501710+zvdxc@users.noreply.github.com>
2025-02-17 16:31:02 +0100

    Update umbrel.sh

--- umbrel.sh
+++ umbrel.sh
@@ -3,4 +3,4 @@ cd umbrel
 wget https://raw.githubusercontent.com/dockur/umbrel/refs/heads/master/compose.yml
 mv compose.yml docker-compose.yml
 echo docker compose up > start
-docker compose up
+docker-compose up
